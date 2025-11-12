d3e5d918ce08d18407c05bef7fbdb5149da94a13
mvukadinoff <mihail.vukadinoff@itgix.com>
2025-11-12 11:47:50 +0200

    support for multiple ECR provision

--- ecr.tf
+++ ecr.tf
@@ -1,24 +1,36 @@
 module "ecr" {
   source  = "terraform-aws-modules/ecr/aws"
-  version = "2.2.0"
+  version = "3.1.0"
 
-  create_repository = var.ecr_create_repository
-  repository_name = var.ecr_repository_name == "" ? "ecr-${local.name_string}" : var.ecr_repository_name
-  repository_type = var.ecr_repository_type
-  repository_read_write_access_arns = var.ecr_repository_read_write_access_arns
-  repository_read_access_arns = var.ecr_repository_read_access_arns
-  repository_encryption_type = var.ecr_repository_encryption_type
+  for_each = local.ecr_input
 
-  repository_image_scan_on_push = var.ecr_repository_image_scan_on_push
-  repository_image_tag_mutability = var.ecr_repository_image_tag_mutability
+  # Use ecr_enabled directly
+  create_repository = var.ecr_enabled
 
-  manage_registry_scanning_configuration = var.ecr_manage_registry_scanning_configuration
-  registry_scan_type = var.ecr_registry_scan_type
-  registry_scan_rules = var.ecr_registry_scan_rules
+  # Compose repo name; optional project prefix
+  repository_name = var.ecr_prefix_with_projectname
+    ? "${var.project_name}-${each.value}"
+    : each.value
 
-  create_lifecycle_policy = var.ecr_create_lifecycle_policy
+  repository_type                         = var.ecr_repository_type
+  repository_read_write_access_arns       = var.ecr_repository_read_write_access_arns
+  repository_read_access_arns             = var.ecr_repository_read_access_arns
+  repository_encryption_type              = var.ecr_repository_encryption_type
+  repository_image_scan_on_push           = var.ecr_repository_image_scan_on_push
+  repository_image_tag_mutability         = var.ecr_repository_image_tag_mutability
+  manage_registry_scanning_configuration  = var.ecr_manage_registry_scanning_configuration
+  registry_scan_type                      = var.ecr_registry_scan_type
+  registry_scan_rules                     = var.ecr_registry_scan_rules
+  create_lifecycle_policy                 = var.ecr_create_lifecycle_policy
 
-  tags = var.resources_tags
-
+  tags = merge(
+    var.resources_tags,
+    {
+      "component"  = "ecr"
+      "env"        = var.environment
+      "name"       = local.name_string
+      "repo-base"  = each.value
+    }
+  )
 }
 

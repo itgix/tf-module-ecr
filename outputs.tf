ad9d82f0a6738e5d622024b067bd3e6ed9b35893
mvukadinoff <mihail.vukadinoff@itgix.com>
2025-11-12 12:58:38 +0200

    renable outputs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 ################################################################################
 # Private Repository
 ################################################################################
-# output "repository_names" {
-#   description = "List of ECR repository names"
-#   value       = [for m in values(module.ecr) : m.repository_name]
-# }
-# output "repository_urls_map" {
-#   description = "Map of repository URLs keyed by logical name"
-#   value       = { for k, m in module.ecr : k => m.repository_url }
-# }
+output "repository_names" {
+  description = "List of ECR repository names"
+  value       = [for m in values(module.ecr) : m.repository_name]
+}
+output "repository_urls_map" {
+  description = "Map of repository URLs keyed by logical name"
+  value       = { for k, m in module.ecr : k => m.repository_url }
+}
